ec68ac8f3ca00529f31b992cca401134b68672fb
Curtis Poe <ovid@cpan.org>
2009-05-24 17:55:36 +0100

    Add the database and the unification modules.

---
+++ lib/AI/Logic/Database.pm
@@ -0,0 +1,171 @@
+package AI::Logic::Database;
+
+use warnings;
+use strict;
+
+use Carp 'croak';
+use AI::Logic::Var 'Var';
+use AI::Logic::Unification ':all';
+
+=head1 NAME
+
+AI::Logic::Database - Logic variables
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+ package My::Database;
+ use AI::Logic::Database
+   variables  => qw(PERSON),
+   predicates => qw(
+   male/1
+   acts/1
+   actor/2
+ );
+ AI::Logic::Database->add_to_database(
+     male('frank');
+     male('barney');
+     acts('frank');
+ );
+
+=cut 
+
+my %DATABASE;
+
+sub import {
+    my ( $class, %arg_for ) = @_;
+
+    # XXX assert that values are arrays
+    my $callpack = caller(0);
+    if ( my $variables = delete $arg_for{variables} ) {
+        foreach my $variable (@$variables) {
+            no strict 'refs';
+
+            # is this enough?
+            *{"$callpack\::$variable"} = sub { Var };
+        }
+    }
+    my $predicates = delete $arg_for{predicates}
+      or die "No predicates found for database ($callpack)";
+
+    my %database;
+    my %installed;
+    foreach my $predicate (@$predicates) {
+        my ( $name, $arity ) = split /\// => $predicate;
+        $database{$name}{$arity}{data} ||= [];
+
+        if ( not $installed{$name} ) {
+            no strict 'refs';
+            *{"$callpack\::$name"} = sub {
+                my $arity = @_;
+                unless ( exists $database{$name}{$arity} ) {
+                    croak "Predicate $name/$arity not found in database";
+                }
+                push @{ $database{$name}{$arity}{data} } => [@_];    # XXX maybe Clone
+            };
+            $installed{$name} = 1;
+        }
+        # XXX assert the name can be a function and that the arity is a
+        # non-negative integer (what do foo/0 mean in this context?)
+
+        if ( 1 == $arity ) {
+
+            # micro-optimization for the win! ;)
+            $database{$name}{$arity}{unification} = sub {
+                my ( $var, $continuation ) = @_;
+                foreach my $data ( @{ $database{$name}{$arity}{data} } ) {
+                    unify( @$data, $var, $continuation );
+                }
+            };
+        }
+        else {
+            $database{$name}{$arity}{unification} = sub {
+                my $continuation = pop @_;
+                foreach my $data ( @{ $database{$name}{$arity}{data} } ) {
+
+                    # $data should be an array reference
+                    unify_all( $data, [@_], $continuation );
+                }
+            };
+        }
+    }
+    $DATABASE{$callpack} = \%database;
+
+    if ( my @keys = keys %arg_for ) {
+        local $" = ', ';
+        croak "Unknown keys passed to AI::Logic::Database (@keys)";
+    }
+}
+
+sub get_database {
+    my $name = shift;
+    return $DATABASE{$name}
+      or croak("No such database ($name)");
+}
+
+=head1 AUTHOR
+
+Curtis "Ovid" Poe, C<< <ovid at cpan.org> >>
+
+Based on the excellent "Perl and Prolog and Continuations... oh my!" by Adrian
+Howard.  L<http://www.perlmonks.org/index.pl?node_id=193649>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-ai-logic at rt.cpan.org>,
+or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=AI-Logic>.  I will be
+notified, and then you'll automatically be notified of progress on your bug as
+I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc AI::Logic::Database
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=AI-Logic>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/AI-Logic>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/AI-Logic>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/AI-Logic/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Curtis "Ovid" Poe, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1;    # End of AI::Logic::Database
---
+++ lib/AI/Logic/Unification.pm
@@ -0,0 +1,137 @@
+package AI::Logic::Unification;
+
+use warnings;
+use strict;
+
+use aliased 'AI::Logic::Var';
+
+use base 'Exporter';
+our @EXPORT_OK = qw(
+    unify
+    unify_all
+);
+our %EXPORT_TAGS = (
+    all => \@EXPORT_OK,
+);
+
+=head1 NAME
+
+AI::Logic::Unification - Logic unification
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 EXPORT
+
+All functions are exportable on demand, or with the tag ':all'.
+
+=head1 FUNCTIONS
+
+=head2 unify
+
+=cut
+
+sub unify {
+    my ( $v1, $v2, $continuation ) = @_;
+    $v1 = Var->new($v1) unless UNIVERSAL::isa( $v1, Var );
+    $v2 = Var->new($v2) unless UNIVERSAL::isa( $v2, Var );
+    if ( $v1->equal($v2) ) {
+        $continuation->();
+    }
+    elsif ( $v1->bind($v2) ) {
+        $continuation->();
+        $v1->unbind;
+    }
+    elsif ( $v2->bind($v1) ) {
+        $continuation->();
+        $v2->unbind;
+    }
+    else {
+        # ???
+    }
+    return;
+}
+
+=head2 unify_all
+
+=cut
+
+sub unify_all {
+    my ( $a, $b, $continuation ) = @_;
+    if ( @$a == 0 && @$b == 0 ) {
+        $continuation->();
+    }
+    elsif ( @$a == @$b ) {
+        my ( $v1, $v2 ) = ( shift @$a, shift @$b );
+        unify( $v1, $v2, sub { unify_all( $a, $b, $continuation ) } );
+        unshift @$a, $v1;
+        unshift @$b, $v2;
+    }
+    return (0);
+}
+
+
+=head1 AUTHOR
+
+Curtis "Ovid" Poe, C<< <ovid at cpan.org> >>
+
+Based on the excellent "Perl and Prolog and Continuations... oh my!" by Adrian
+Howard.  L<http://www.perlmonks.org/index.pl?node_id=193649>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-ai-logic at rt.cpan.org>,
+or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=AI-Logic>.  I will be
+notified, and then you'll automatically be notified of progress on your bug as
+I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc AI::Logic::Unification
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=AI-Logic>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/AI-Logic>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/AI-Logic>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/AI-Logic/>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+Adrian Howard inspired this.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Curtis "Ovid" Poe, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1;    # End of AI::Logic::Unification
